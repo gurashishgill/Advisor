74b14e125e818ec34695c033e18babc4454a84a6
Gurashish Singh Gill <gurashish.gill@incedoinc.com>
2023-02-21 14:52:30 +0530

    gurashish ui change

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
@@ -23,41 +21,49 @@ function NavigationBar() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="/">
+        {/* <Navbar.Brand href="/">
           <img src={logo} style={{ height: "40px" }} />
-        </Navbar.Brand>
+        </Navbar.Brand> */}
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            // style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            {userData === null ? (
-              <>
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/about">About Us</Nav.Link>
-                <NavDropdown title="Login" id="navbarScrollingDropdown">
-                  <NavDropdown.Item href="/AdvisorLogin">
-                    Login as Advisor
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="/ClientLogin">
-                    Login as Client
-                  </NavDropdown.Item>
-                </NavDropdown>
-                <Nav.Link href="/AdvisorRegister">Signup</Nav.Link>
-              </>
-            ) : (
-              <>
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/about">About Us</Nav.Link>
-                <Nav.Link className="logout_button" onClick={handleLogout}>
-                  Logout <FiLogOut />
-                </Nav.Link>
-              </>
-            )}
-          </Nav>
+          {userData === null ? (
+            <>
+              <Nav className="me-auto my-2 my-lg-0 nav_container" navbarScroll>
+                <div className="nav_item_1">
+                  <Nav.Link href="/">
+                    <img src={logo} style={{ height: "60px" }} />
+                  </Nav.Link>
+                </div>
+                <div className="nav_item_2">
+                  <NavDropdown title="Login" id="navbarScrollingDropdown">
+                    <NavDropdown.Item href="/AdvisorLogin">
+                      Login as Advisor
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="/ClientLogin">
+                      Login as Client
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                  <Nav.Link href="/AdvisorRegister">Signup</Nav.Link>
+                </div>
+              </Nav>
+            </>
+          ) : (
+            <>
+              <Nav className="me-auto my-2 my-lg-0 nav_container" navbarScroll>
+                <div className="nav_item_1">
+                  <Nav.Link href="/">
+                    <img src={logo} style={{ height: "40px" }} />
+                  </Nav.Link>
+                </div>
+                <div className="nav_item_2">
+                  <Nav.Link className="logout_button" onClick={handleLogout}>
+                    Logout <FiLogOut />
+                  </Nav.Link>
+                </div>
+              </Nav>
+            </>
+          )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
